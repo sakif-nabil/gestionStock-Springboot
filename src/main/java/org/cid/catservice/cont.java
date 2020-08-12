ca55f8a23229d687b8056c806eabb5b0e7c1464e
sakif-nabil <53219441+sakif-nabil@users.noreply.github.com>
2020-08-12 12:14:36 +0100

    add controller to say hello

---
+++ src/main/java/org/cid/catservice/cont.java
@@ -0,0 +1,13 @@
+package org.cid.catservice;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class cont {
+    @RequestMapping("/")
+    public String sayhi(){
+        return"Hello, I am SAKIF Nabil from morocco. I am a software engineer";
+    }
+}
